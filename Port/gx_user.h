f34ba08c6818c968df87117f86b755d60b99e150
HelloByeAll <1139077247@qq.com>
2020-11-20 20:53:35 +0800

    V1.0.0

---
+++ Port/gx_user.h
@@ -0,0 +1,39 @@
+/*
+*********************************************************************************************************
+*
+*	模块名称 : 头文件汇总
+*	文件名称 : tx_user.h
+*	版    本 : V1.0
+*	说    明 : GUIX当前使用头文件汇总
+*
+*	修改记录 :
+*		版本号    日期        作者     说明
+*		V1.0    2020-06-04  Eric2013   首次发布
+*
+*	Copyright (C), 2019-2030, 安富莱电子 www.armfly.com
+*
+*********************************************************************************************************
+*/
+
+#ifndef __TX_USER_H
+#define __TX_USER_H
+#define GX_INCLUDE_USER_DEFINE_FILE
+#define GX_DISABLE_THREADX_BINDING
+#define GUIX_BINDING_RT_THREAD
+//#include "tx_user.h"
+#include "gx_api.h"
+
+/*
+************************************************************************
+*						宏定义
+************************************************************************
+*/
+// defined GX_DISABLE_SOFTWARE_DECODER_SUPPORT
+// defined GX_DISABLE_ARC_DRAWING_SUPPORT
+
+#define GX_ARC_DRAWING_SUPPORT
+
+
+#endif
+
+/***************************** 安富莱电子 www.armfly.com (END OF FILE) *********************************/
